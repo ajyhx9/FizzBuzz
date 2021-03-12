0084ca40cca2382a74242d6720bf2e1e7689fac7
ajyhx9 <54514665+ajyhx9@users.noreply.github.com>
2021-03-12 17:58:54 -0600

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,16 +1,11 @@
 i = 0
 
-while i < 100 :
+while i < 100:
     i += 1
-    if i%3 == 0 and i%5 == 0:
-        print("FizzBuzz")
-    elif i%3 == 0:
-        print("Fizz")
-    elif i%5 == 0:
-        print("Buzz")
-    else:
-        print(i)
-      
-        
-        
-
+    if i%3 == 0:
+        print("Fizz", end="")
+    if i%5 == 0:
+        print("Buzz", end="")
+    if i%3 != 0 and i%5 != 0:
+        print(i, end="")
+    print("\n")
